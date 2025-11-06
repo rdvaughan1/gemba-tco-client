0cc401cef910bc8445012f3418c2d2b1a6ee5f27
rdvaughan1 <rickvaughan@me.com>
2025-11-05 17:01:34 -0700

    Update vehiclesCreate.js

--- functions/vehiclesCreate.js
+++ functions/vehiclesCreate.js
@@ -1,1 +1,16 @@
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const { connectDB } = require('../lib/db');
+const Vehicle = require('../models/Vehicle');
 
+
+exports.handler = async function(event) {
+await connectDB();
+const vehicleData = JSON.parse(event.body);
+const vehicle = new Vehicle(vehicleData);
+const created = await vehicle.save();
+return {
+statusCode: 201,
+body: JSON.stringify(created)
+};
+};
