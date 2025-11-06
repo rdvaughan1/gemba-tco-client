3f255fbff1145e809f60a80b5eaa7a342891f837
rdvaughan1 <rickvaughan@me.com>
2025-11-05 17:03:01 -0700

    Update Vehicle.js

--- models/Vehicle.js
+++ models/Vehicle.js
@@ -1,1 +1,18 @@
+const mongoose = require('mongoose');
 
+
+const VehicleSchema = new mongoose.Schema({
+acquisitionCost: Number,
+upfittingCost: Number,
+annualFuelCost: Number,
+annualInsuranceCost: Number,
+annualMaintenanceCost: Number,
+ageInYears: Number,
+mileage: Number,
+estimatedResaleValue: Number,
+group: { type: String, default: 'Ungrouped' },
+dateCreated: { type: Date, default: Date.now }
+});
+
+
+module.exports = mongoose.model('Vehicle', VehicleSchema);
